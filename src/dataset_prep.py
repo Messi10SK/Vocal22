77f56be44544687fc3a2808adb7c4d14a42918ef
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2026-05-08 19:01:48 +0530

    dataset prep process

--- src/dataset_prep.py
+++ src/dataset_prep.py
@@ -1,32 +1,66 @@
 import numpy as np
 import glob
 
-def main ():
+def main():
+
     for dstype in ['train', 'test']:
 
-        print(f'Processing {dstype} data...')
-        dstype_paths = glob.glob(f'./processed_data/{dstype}/*.npz')
+        print(f'\n🔥 Processing {dstype} data...')
+
+        dstype_paths = sorted(
+            glob.glob(f'./processed_data/{dstype}/*.npz')
+        )
 
         mix_mags_dstype = []
         mix_phases_dstype = []
         vocal_mags_dstype = []
 
         for i, path in enumerate(dstype_paths):
+
             print(f'Processing {i+1}/{len(dstype_paths)}')
-            data = np.load(path, mmap_mode='r')
-            mix_mags_dstype = mix_mags_dstype + [data['mix_mag'][:,:,:,np.newaxis]]
-            mix_phases_dstype = mix_phases_dstype + [data['mix_phase'][:,:,:,np.newaxis]]
-            vocal_mags_dstype = vocal_mags_dstype + [data['vocal_mag'][:,:,:,np.newaxis]]
-            
+
+            data = np.load(path)
+
+            # ============================================
+            # USE SMALLER DATATYPES TO SAVE RAM
+            # ============================================
+
+            mix_mags_dstype.append(
+                data['mix_mag'][:,:,:,np.newaxis].astype(np.float32)
+            )
+
+            mix_phases_dstype.append(
+                data['mix_phase'][:,:,:,np.newaxis].astype(np.complex64)
+            )
+
+            vocal_mags_dstype.append(
+                data['vocal_mag'][:,:,:,np.newaxis].astype(np.float32)
+            )
+
+        print('\n🔥 Concatenating arrays...')
+
         mix_mags_dstype = np.concatenate(mix_mags_dstype, axis=0)
         mix_phases_dstype = np.concatenate(mix_phases_dstype, axis=0)
         vocal_mags_dstype = np.concatenate(vocal_mags_dstype, axis=0)
-        
-        print('Saving data...')
-        # save the data to npy files
-        np.save(f'./processed_data/mix_mags_{dstype}_512x128.npy', mix_mags_dstype)
-        np.save(f'./processed_data/mix_phases_{dstype}_512x128.npy', mix_phases_dstype)
-        np.save(f'./processed_data/vocal_mags_{dstype}_512x128.npy', vocal_mags_dstype)
-        
+
+        print('\n🔥 Saving datasets...')
+
+        np.save(
+            f'./processed_data/mix_mags_{dstype}_512x128.npy',
+            mix_mags_dstype
+        )
+
+        np.save(
+            f'./processed_data/mix_phases_{dstype}_512x128.npy',
+            mix_phases_dstype
+        )
+
+        np.save(
+            f'./processed_data/vocal_mags_{dstype}_512x128.npy',
+            vocal_mags_dstype
+        )
+
+        print(f'\n✅ {dstype} dataset saved successfully')
+
 if __name__ == '__main__':
     main()
